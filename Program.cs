2694fc81f5e9b1cb06c9fd3ca8c050a218452e9d
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-10-05 09:33:30 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace app;
+class Program
+{
+    //UserInput ui = new UserInput();
+    static void Main(String[] args)
+    {
+        UserInput ui = new UserInput();
+        ui.ReadLineDemo();
+
+        
+    }
+}
---
+++ UserInput.cs
@@ -0,0 +1,8 @@
+//namespace app;
+class UserInput{
+    public void ReadLineDemo() {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Good Morning " + name + "! ");
+    }
+}
